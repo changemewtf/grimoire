b97a8f57719e9134fdd112c316792863af96b07f
Max Cantor <max@maxcantor.net>
2017-01-23 16:54:37 -0500

    Delegate and freeze data objects.

--- lib/grimoire/data_object.rb
+++ lib/grimoire/data_object.rb
@@ -6,17 +6,21 @@ module Grimoire
 
     def initialize attributes
       @attributes = {}
+
       attributes.each do |key, value|
         varname = "@#{key}".to_sym
+        next if instance_variable_get(varname) && key.is_a?(String)
         instance_variable_set varname, value
         define_singleton_method key.to_sym do
           instance_variable_get varname
         end
         @attributes[key] = value
       end
+
+      freeze
     end
 
-    def_delegators %i[@attributes each keys values]
+    def_delegators *%i[@attributes each keys values]
 
     def [] name
       instance_variable_get "@#{name}".to_sym
--- spec/data_object_spec.rb
+++ spec/data_object_spec.rb
@@ -26,4 +26,29 @@ RSpec.describe "data objects" do
   it "[string] -> method" do
     expect(DataObject.new("hey" => "you")).to have_attributes(hey: "you")
   end
+
+  it "cannot be changed" do
+    violation = -> do
+      DataObject.new("hey" => "you").instance_eval do
+        @hey = "um"
+      end
+    end
+    expect(violation).to raise_error RuntimeError
+  end
+
+  it "acts like a hash" do
+    expect(DataObject.new(hey: "you").keys).to eq [:hey]
+    expect(DataObject.new(hey: "you").values).to eq ["you"]
+  end
+
+  it "symbols win when duplicate keys" do
+    o = DataObject.new "hey" => "me", :hey => "you"
+    expect(o.hey).to eq "you"
+    expect(o[:hey]).to eq "you"
+    expect(o["hey"]).to eq "you"
+    o = DataObject.new :hey => "you", "hey" => "me"
+    expect(o.hey).to eq "you"
+    expect(o[:hey]).to eq "you"
+    expect(o["hey"]).to eq "you"
+  end
 end
