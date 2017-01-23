f83afa9bf51c2af88f39bd82c5953fb0e4b254c5
Max Cantor <max@maxcantor.net>
2017-01-23 17:35:24 -0500

    Add epoch time contract

--- lib/grimoire/contracts.rb
+++ lib/grimoire/contracts.rb
@@ -5,10 +5,16 @@ require "grimoire/contract"
 module Grimoire
   ContractViolation = Class.new Exception
 
+  DATE_METHODS = %i[day month year]
+  TIME_METHODS = %i[hour min sec zone]
+  EPOC_METHODS = DATE_METHODS + TIME_METHODS
+
   module Contracts
     Text = Contract.new %i[upcase start_with?], -> { "" }
     List = Contract.new %i[size push pop],      -> { ::Array.new }
-    Date = Contract.new %i[day month year],     -> { ::Date.today }
+    Date = Contract.new DATE_METHODS,           -> { ::Date.today }
+    Time = Contract.new TIME_METHODS,           -> { ::Time.now }
+    Epoc = Contract.new EPOC_METHODS,           -> { ::Time.now }
      Int = Contract.new %i[+ - > <],            -> { 0 }
     Hash = Contract.new %i[keys values fetch],  -> { ::Hash.new }
   end
