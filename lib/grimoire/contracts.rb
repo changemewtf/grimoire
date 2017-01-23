3ffae02bb183a057ec41e6733c98b7ab1a6b846d
Max Cantor <max@maxcantor.net>
2017-01-23 17:26:24 -0500

    Add int and hash contracts.

--- lib/grimoire/contracts.rb
+++ lib/grimoire/contracts.rb
@@ -7,7 +7,9 @@ module Grimoire
 
   module Contracts
     Text = Contract.new %i[upcase start_with?], -> { "" }
-    List = Contract.new %i[size push pop],      -> { [] }
+    List = Contract.new %i[size push pop],      -> { ::Array.new }
     Date = Contract.new %i[day month year],     -> { ::Date.today }
+     Int = Contract.new %i[+ - > <],            -> { 0 }
+    Hash = Contract.new %i[keys values fetch],  -> { ::Hash.new }
   end
 end
--- spec/contracts_spec.rb
+++ spec/contracts_spec.rb
@@ -1,19 +1,29 @@
 require "grimoire/contracts"
 
+Fizzer = Grimoire::Contract.new %i[fizz], -> { "fizzy" }
+
 RSpec.describe "contracts" do
   it "sets contract-based defaults" do
-    expect(Grimoire::Contracts::List.default.size).to eq 0
+    expect(Fizzer.default).to eq "fizzy"
   end
 
   it "enforces contracts" do
-    expect(-> { Grimoire::Contracts::List.check "" }).to raise_error Grimoire::ContractViolation
+    expect(-> { Fizzer.check "" }).to raise_error Grimoire::ContractViolation
   end
 
-  it "allows casting" do
-    expect(Grimoire::Contracts::Date.check "11/22").to have_attributes(
-      day: 22,
-      month: 11,
-      year: Date.today.year
-    )
+  context "dates" do
+    it "allows casting" do
+      expect(Grimoire::Contracts::Date.check "11/22").to have_attributes(
+        day: 22,
+        month: 11,
+        year: Date.today.year
+      )
+    end
+  end
+
+  context "integers" do
+    it "defaults to zero" do
+      expect(Grimoire::Contracts::Int.default).to eq 0
+    end
   end
 end
